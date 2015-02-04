a54b5cb4a2a1a227aef730f7e02d4abfaa47b282
larsmh <lars.mh23@gmail.com>
2015-02-04 16:45:36 +0100

    adapter set in onResume()

--- src/com/insider/kontrollactiveDatabase/RegisterJob.java
+++ src/com/insider/kontrollactiveDatabase/RegisterJob.java
@@ -32,26 +32,6 @@ public class RegisterJob extends AsyncTask<String, Integer, Long>{
         } catch (Exception e) {
             Log.d("!!!", e.getLocalizedMessage());
         }
-		
-		/* NTNU database
-		Connection con=null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			con = DriverManager.getConnection(params[0], params[1], params[2]);
-	
-			Statement stmt = null;
-	        stmt = con.createStatement();
-	        stmt.executeUpdate(params[3]);
-	        
-	        con.close();
-	    }catch(SQLException e){
-	        	e.printStackTrace();
-	    }*/
 		return null;
 	}
 
